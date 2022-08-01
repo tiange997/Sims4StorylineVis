ef74b6662152df8326a9d7a84a36ecce4c0ce574
letianwang <wangl8@tcd.ie>
2022-08-01 10:59:34 +0200

    Update for online server

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,30 @@ const WebpackBar = require('webpackbar')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './app/static/index.html',
-  filename: 'index.html',
+  template: './app/static/simple11.html',
+  filename: 'simple11.html',
   inject: 'body',
+  chunks: ['simple11'],
 })
 
 module.exports = {
   context: path.resolve(__dirname),
   devtool: 'source-map',
-  entry: './app/js/app.js',
+  entry: {
+    simple11: ['./app/js/simple11.js'],
+    complex11: ['./app/js/complex11.js'],
+    complex5: ['./app/js/complex5.js'],
+    simple5: ['./app/js/simple5.js'],
+    complex16: ['./app/js/complex16.js'],
+    simple16: ['./app/js/simple16.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
+    // filename: 'index_bundle.js',
+    filename: '[name].js',
+    chunkFilename: '[id].[chunkhash].js',
   },
+  devServer: {},
   mode: 'production',
   module: {
     rules: [
@@ -31,5 +42,38 @@ module.exports = {
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
   },
-  plugins: [HtmlWebpackPluginConfig, new WebpackBar()],
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new HtmlWebpackPlugin({
+      template: './app/static/simple11.html',
+      filename: 'complex11.html',
+      inject: 'body',
+      chunks: ['complex11'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './app/static/simple5.html',
+      filename: 'simple5.html',
+      inject: 'body',
+      chunks: ['simple5'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './app/static/complex5.html',
+      filename: 'complex5.html',
+      inject: 'body',
+      chunks: ['complex5'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './app/static/complex16.html',
+      filename: 'complex16.html',
+      inject: 'body',
+      chunks: ['complex16'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './app/static/simple16.html',
+      filename: 'simple16.html',
+      inject: 'body',
+      chunks: ['simple16'],
+    }),
+    new WebpackBar(),
+  ],
 }
