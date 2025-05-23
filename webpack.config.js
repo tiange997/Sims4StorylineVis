a8b486991406513e97109d807d63546790aabf72
tiange997 <61433528+tiange997@users.noreply.github.com>
2025-05-23 10:46:06 +0200

    Initial Adaptation for Sims 4

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,6 @@ module.exports = {
   entry: {
     simple11: ['./app/js/simple11.js'],
     complex11: ['./app/js/complex11.js'],
-    complex5: ['./app/js/complex5.js'],
-    simple5: ['./app/js/simple5.js'],
-    complex16: ['./app/js/complex16.js'],
-    simple16: ['./app/js/simple16.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -51,30 +47,6 @@ module.exports = {
       filename: 'complex11.html',
       inject: 'body',
       chunks: ['complex11'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './app/static/simple5.html',
-      filename: 'simple5.html',
-      inject: 'body',
-      chunks: ['simple5'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './app/static/complex5.html',
-      filename: 'complex5.html',
-      inject: 'body',
-      chunks: ['complex5'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './app/static/complex16.html',
-      filename: 'complex16.html',
-      inject: 'body',
-      chunks: ['complex16'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './app/static/simple16.html',
-      filename: 'simple16.html',
-      inject: 'body',
-      chunks: ['simple16'],
     }),
     new WebpackBar(),
   ],
